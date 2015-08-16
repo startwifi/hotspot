69e426da7a79549679c5d569619dd616d936d308
Max Grechko <max@grechko.org>
2015-08-16 04:15:28 +0300

    Turn off registrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admins, path: '/', path_names: { sign_in: 'login', sign_up: 'new',
-    sign_out: 'logout', password: 'secret', registration: 'register' }
+    sign_out: 'logout', password: 'secret', registration: 'profile' }
+  as :admin do
+    get '/profile/edit', to: 'devise/registrations#edit',   as: 'edit_admin_registration'
+    put '/profile',      to: 'devise/registrations#update', as: 'admin_registration'
+  end
   root to: 'home#index'
   resources :users
   get '/auth/:provider/callback', to: 'sessions#create'
