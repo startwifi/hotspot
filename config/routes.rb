b3b492b62892e8f6d3e504c9930e6f9a4db9e9a1
Max Grechko <max@grechko.org>
2015-08-17 19:25:09 +0300

    Fix an edit form for user settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :admins, path: '/', path_names: { sign_in: 'login', sign_up: 'new',
-    sign_out: 'logout', password: 'secret', registration: 'profile' }
+  devise_for :admins,
+    path: '/', 
+    path_names: {
+      sign_in: 'login',
+      sign_up: 'new',
+      sign_out: 'logout',
+      password: 'secret',
+      registration: 'profile'
+    }
+
   as :admin do
     get '/profile/edit', to: 'devise/registrations#edit',   as: 'edit_admin_registration'
     put '/profile',      to: 'devise/registrations#update', as: 'admin_registration'
   end
+
   root to: 'home#index'
   resource  :dashboard, only: :show
   resources :users
