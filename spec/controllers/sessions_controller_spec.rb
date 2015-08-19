3c61c66aa17ccf17d4700484b9b70cd776e795ff
Max Grechko <max@grechko.org>
2015-08-19 09:27:24 +0300

    Add vk tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,29 +2,53 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
-  before do
-    request.env['omniauth.auth'] = auth_mock
-  end
+  describe 'GET #create' do
+    context 'facebook' do
+      before do
+        request.env['omniauth.auth'] = auth_mock_facebook
+      end
 
-  describe '#create' do
-    it 'creates a user' do
-      expect {post :create, provider: :facebook}.to change{ User.count }.by(1)
+      it 'creates a user' do
+        expect {post :create, provider: :facebook}.to change{ User.count }.by(1)
+      end
+
+      it 'creates a session' do
+        expect(session[:user_id]).to be_nil
+        post :create, provider: :facebook
+        expect(session[:user_id]).not_to be_nil
+      end
+
+      it 'redirects to the social page' do
+        post :create, provider: :facebook
+        expect(response).to redirect_to social_url
+      end
     end
 
-    it 'creates a session' do
-      expect(session[:user_id]).to be_nil
-      post :create, provider: :facebook
-      expect(session[:user_id]).not_to be_nil
-    end
+    context 'with vkontakte' do
+      before do
+        request.env['omniauth.auth'] = auth_mock_vkontakte
+      end
 
-    it 'redirects to the social page' do
-      post :create, provider: :facebook
-      expect(response).to redirect_to social_url
+      it 'creates a user' do
+        expect {post :create, provider: :vkontakte}.to change{ User.count }.by(1)
+      end
+
+      it 'creates a session' do
+        expect(session[:user_id]).to be_nil
+        post :create, provider: :vkontakte
+        expect(session[:user_id]).not_to be_nil
+      end
+
+      it 'redirects to the social page' do
+        post :create, provider: :vkontakte
+        expect(response).to redirect_to social_url
+      end
     end
   end
 
   describe '#destroy' do
     before do
+      request.env['omniauth.auth'] = auth_mock_facebook
       post :create, provider: :facebook
     end
 
--- spec/support/helpers/omniauth.rb
+++ spec/support/helpers/omniauth.rb
@@ -1,12 +1,38 @@
 module Omniauth
 
   module Mock
-    def auth_mock
+    def auth_mock_facebook
       OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
         'provider' => 'facebook',
         'uid' => '123545',
         'info' => {
-          'name' => 'mockuser'
+          'name' => 'mockuser',
+          'urls' => { 'Facebook' => 'http://facebook.com/uid/12345678' }
+        },
+        'extra' => {
+          'raw_info' => {
+            'birthday' => '8/24/1991'
+          }
+        },
+        'credentials' => {
+          'token' => 'mock_token',
+          'secret' => 'mock_secret'
+        }
+      })
+    end
+
+    def auth_mock_vkontakte
+      OmniAuth.config.mock_auth[:vkontakte] = OmniAuth::AuthHash.new({
+        'provider' => 'vkontakte',
+        'uid' => '123545',
+        'info' => {
+          'name' => 'mockuser',
+          'urls' => { 'Vkontakte' => 'http://vk.com/username' }
+        },
+        'extra' => {
+          'raw_info' => {
+            'birthday' => '8/24/1991'
+          }
         },
         'credentials' => {
           'token' => 'mock_token',
@@ -20,14 +46,14 @@ module Omniauth
     def sign_in_facebook
       visit social_path
       expect(page).to have_content 'Facebook'
-      auth_mock
+      auth_mock_facebook
       click_link 'Facebook'
     end
 
     def sign_in_vk
       visit social_path
       expect(page).to have_content 'Vkontakte'
-      auth_mock
+      auth_mock_vkontakte
       click_link 'Vkontakte'
     end
 
