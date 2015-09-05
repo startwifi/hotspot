47145f5b38826e52e71c32f1bfd04ea381dcb8d2
Max Grechko <max@grechko.org>
2015-09-05 12:44:23 +0300

    Refactor Events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,23 +1,31 @@
 class EventsController < ApplicationController
   def subscribe
-    current_user.add_event(:subscribe)
-    redirect_to router_url
+    case params[:provider]
+    when 'fb'
+      current_user.add_event(:like)
+      redirect_to router_url
+    when 'vk'
+      current_user.add_event(:subscribe)
+      redirect_to router_url
+    end
   end
 
   def post
-    if params[:provider] == 'vk'
-      current_user.add_event(:post)
-      redirect_to router_url
-    elsif params[:provider] == 'fb'
+    case params[:provider]
+    when 'fb'
       graph = Koala::Facebook::API.new(session[:user_access_token])
       share = graph.put_wall_post(current_user.company.fb.post_text,
         { link: current_user.company.fb.post_link,
         description: current_user.company.fb.post_text })
       if share['id']
+        current_user.add_event(:share)
         redirect_to router_url
       else
         render text: 'Error'
       end
+    when 'vk'
+      current_user.add_event(:post)
+      redirect_to router_url
     end
   end
 end
