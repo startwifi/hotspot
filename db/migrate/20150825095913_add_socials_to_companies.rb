2c35dc3506e6492ac39d41df557c369046b8c7a5
Max Grechko <max@grechko.org>
2015-09-09 13:12:07 +0300

    Remove socials from companies

--- db/migrate/20150825095913_add_socials_to_companies.rb
+++
@@ -1,7 +0,0 @@
-class AddSocialsToCompanies < ActiveRecord::Migration
-  def change
-    add_column :companies, :fb, :boolean, default: false
-    add_column :companies, :vk, :boolean, default: false
-    add_column :companies, :twitter, :boolean, default: false
-  end
-end
