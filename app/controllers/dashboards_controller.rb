c8e01c3184b839a5cc1aaf3d45588c1291cd7acc
Max Grechko <max@grechko.org>
2015-09-11 18:21:29 +0300

    Add simple statistics

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,6 +4,9 @@ class DashboardsController < ApplicationController
   def show
     unless current_admin.admin?
       @recent_events = current_admin.company.events.order('created_at DESC').first(10)
+      @company = current_admin.company
+      @days_range = (Time.now.beginning_of_month.to_date..Time.now.to_date).to_a.reverse
+      @events = current_admin.company.events.month
     else
       redirect_to companies_path
     end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ActiveRecord::Base
   belongs_to :user
+
+  scope :today, ->{ select('*').joins(:user).where('events.created_at >= ?', Time.zone.now.beginning_of_day) }
+  scope :month, ->{ select('*').joins(:user).where('events.created_at >= ?', Time.zone.now.beginning_of_month) }
+  scope :social_count, ->(social){ where('users.provider = ?', social).count }
+  scope :by_date, ->(date){ where('events.created_at::date = ?', date.to_date) }
 end
