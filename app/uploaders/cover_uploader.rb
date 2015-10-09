445f051faf75f6c4e98234e53418dd3452fb8ff7
Max Grechko <max@grechko.org>
2015-10-09 11:57:34 +0300

    Refactor uploaders

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -1,40 +1,4 @@
 # encoding: utf-8
 
-class CoverUploader < CarrierWave::Uploader::Base
-
-  include CarrierWave::MiniMagick
-
-  storage :file
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
-
-  def extension_white_list
-    %w(jpg jpeg png)
-  end
-
-  def filename
-    "#{secure_token(10)}.#{file.extension}" if original_filename.present?
-  end
-
-  protected
-
-  def secure_token(length=16)
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
-  end
-
+class CoverUploader < ApplicationUploader
 end
--- app/uploaders/fb_uploader.rb
+++ app/uploaders/fb_uploader.rb
@@ -1,35 +1,4 @@
 # encoding: utf-8
 
-class FbUploader < CarrierWave::Uploader::Base
-
-  include CarrierWave::MiniMagick
-
-  storage :file
-
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  def extension_white_list
-    %w(jpg jpeg png)
-  end
-
-  def filename
-    "#{secure_token(10)}.#{file.extension}" if original_filename.present?
-  end
-
-  protected
-
-  def secure_token(length=16)
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
-  end
-
+class FbUploader < ApplicationUploader
 end
