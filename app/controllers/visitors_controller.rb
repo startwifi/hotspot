ef9fbdad0cf54ac9229aa8c9c039984b5bfb5542
Max Grechko <max@grechko.org>
2015-10-10 19:59:45 +0300

    Change visitors controller

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,23 +3,14 @@ class VisitorsController < ApplicationController
   layout 'visitors'
 
   def index
-    if Rails.env.production? || Rails.env.staging?
-      # render_404 if session[:company_token].blank?
-      token = params[:hs][:token]
-      link  = params[:hs][:link_login_only]
-      dst   = params[:hs][:dst]
-      mac   = params[:hs][:mac_esc]
-    else
-      token = Company.first.token
-      link  = 'http://192.168.1.1/login'
-      dst   = 'http://ukr.net'
-      mac   = 'b8:8d:12:42:03:6e'
+    if params[:hs]
+      session[:company_token] = params[:hs][:token]
+      session[:link] = params[:hs][:link_login_only]
+      session[:dst] = params[:hs][:dst]
+      session[:mac] = params[:hs][:mac_esc]
     end
-    session[:link] = link
-    session[:dst] = dst
-    session[:mac] = mac
-    session[:company_token] = token
-    @company = Company.find_by_token(token)
+    @company = Company.find_by_token(session[:company_token])
+    render_404 unless @company
   end
 
 end
