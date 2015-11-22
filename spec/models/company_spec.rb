265dcb3d3e4212c4792a4b79c12dfa45f6a3fb47
Max Grechko <max@grechko.org>
2015-11-22 16:20:58 +0200

    Add #create_dummy_social tests

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -17,4 +17,46 @@ RSpec.describe Company, type: :model do
     it { should have_one(:ok).dependent(:destroy) }
     it { should have_one(:in).dependent(:destroy) }
   end
+
+  describe '#create_dummy_social' do
+    let!(:company) { create(:company) }
+
+    social_networks = %i(vk tw ok in) # todo: add fb (failed)
+
+    social_networks.each do |sn|
+      context "creates #{sn.capitalize} settings" do
+        before { company.create_dummy_social(sn) }
+
+        subject { company.send(sn) }
+
+        it 'creates social network settings' do
+          expect(subject).not_to be_nil
+        end
+
+        it 'returns a group name' do
+          expect(subject.group_name).to eq ENV["#{sn.upcase}_GROUP_NAME"]
+        end
+
+        it 'returns an action' do
+          expect(subject.action).to eq 'disabled'
+        end
+
+        it 'returns a redirection link' do
+          expect(subject.link_redirect).to eq ENV['LINK_REDIRECT']
+        end
+
+        it 'returns a post text' do
+          expect(subject.post_text).to eq company.name
+        end
+
+        it 'returns a post link' do
+          expect(subject.post_link).to_not be_nil
+        end
+
+        it 'returns a post image' do
+          expect(subject.post_image).to_not be_nil
+        end
+      end
+    end
+  end
 end
