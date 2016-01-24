64c1e6e90639c15374e9dddaf25c6554473129a7
faustman@htpc <andrij.tytar@gmail.com>
2016-01-24 19:18:25 +0200

    Remove junks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,14 +7,6 @@ module ApplicationHelper
     action.include?(params[:action])
   end
 
-  def flash_level(level)
-    case level
-    when :notice then "alert-info"
-    when :error then "alert-error"
-    when :alert then ""
-    end
-  end
-
   def icon_class_by(provider)
     case provider
     when 'vkontakte' then 'vk'
